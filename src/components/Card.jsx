9e52d979908da9f038665db52e95c9110104fbee
Jime Matthies <jimematthies@gmail.com>
2024-01-25 14:33:28 -0300

    Header

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 const Card = () => {
 	return (
 		<article className='product-card'>
-			<img
-				className='product-img'
-				src='../assets/mock1.jpg'
-				alt='Macbook Pro'
-			/>
+			<img className='product-img' src='assets/mock1.jpg' alt='Macbook Pro' />
 			<div className='product-info'>
 				<span className='product-title'>Macbook Pro 15&apos;4</span>
 				<span className='product-description'>Space Gray</span>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,60 @@ const Header = () => {
 	return (
 		<header>
 			<div className='container-fluid'>
-				<h1>Header</h1>
+				<div className='search'>
+					<div className='logo'>
+						<img
+							src='../assets/tiendamia-logo.svg'
+							width='218'
+							alt='Logo store'
+						/>
+					</div>
+					<div className='form'>
+						<form>
+							<input type='text' placeholder='Search' />
+						</form>
+					</div>
+					<div className='social'>
+						<ul>
+							<li>
+								<img
+									src='../assets/facebook.png'
+									width='25'
+									alt='Facebook Logo'
+								/>
+							</li>
+							<li>
+								<img
+									src='../assets/instagram.png'
+									width='25'
+									alt='Instagram Logo'
+								/>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className='nav'>
+					<ul>
+						<li>
+							<a href='#'>Ofertas de la semana</a>
+						</li>
+						<li>
+							<a href='#'>Cómo comprar</a>
+						</li>
+						<li>
+							<a href='#'>Costos y Tarifas</a>
+						</li>
+						<li>
+							<a href='#'>Mis pedidos</a>
+						</li>
+						<li>
+							<a href='#'>Invita y Gana</a>
+						</li>
+						<li>
+							<a href='#'>Garantía de entrega</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</header>
 	);
