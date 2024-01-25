ac9adef5e50809bec4d8fdc953e1d666985ab9c3
Jime Matthies <jimematthies@gmail.com>
2024-01-25 13:50:51 -0300

    Hero section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Card = () => {
-	return <div className='container-fluid'></div>;
+	return (
+		<article className='product-card'>
+			<img
+				className='product-img'
+				src='../assets/mock1.jpg'
+				alt='Macbook Pro'
+			/>
+			<div className='product-info'>
+				<span className='product-title'>Macbook Pro 15&apos;4</span>
+				<span className='product-description'>Space Gray</span>
+				<div className='product-price-block'>
+					<span className='price'>$750.000</span>
+					<span className='discount'>50% Off</span>
+				</div>
+				<div className='product-tax-policy'>
+					Incluye impuesto País y percepción AFIP
+				</div>
+			</div>
+		</article>
+	);
 };
 
 export default Card;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,232 +1,32 @@
 import React from 'react';
+import Card from '../components/Card';
 
 const Home = () => {
 	return (
 		<>
-			<section>
+			<section id='hero'>
 				<div className='container-fluid'>
-					<h1>Section</h1>
+					<div className='title-container'>
+						<span>tecnología</span>
+						<span>renovada</span>
+					</div>
 				</div>
 			</section>
 			<main>
 				<div className='container-fluid'>
 					<div className='product-container'>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock1.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock2.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock1.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock2.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock1.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock2.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock1.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock2.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock1.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock2.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock1.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
-						<article className='product-card'>
-							<img
-								className='product-img'
-								src='../assets/mock2.jpg'
-								alt='Macbook Pro'
-							/>
-							<div className='product-info'>
-								<span className='product-title'>Macbook Pro 15&apos;4</span>
-								<span className='product-description'>Space Gray</span>
-								<div className='product-price-block'>
-									<span className='price'>$750.000</span>
-									<span className='discount'>50% Off</span>
-								</div>
-								<div className='product-tax-policy'>
-									Incluye impuesto País y percepción AFIP
-								</div>
-							</div>
-						</article>
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
 					</div>
 				</div>
 			</main>
